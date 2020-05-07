3a4361f9a8e2f08cc2b4357164c2b1a57e3bb9fb
unknown <osm0718@gmail.com>
2020-05-07 02:05:12 -0400

    split() action added (and methods updated accordingly. Player actions are now methods instead of being within playerTurn()

--- BlackJack/src/Main.java
+++ BlackJack/src/Main.java
@@ -13,12 +13,16 @@ public class Main {
 	private static Scanner kb = new Scanner(System.in);
 	private static Deck deck = new Deck();
 	private static Player player = new Player();
+	private static Player split1 = new Player();
+	private static Player split2 = new Player();
+	private static Player[] split = {split1, split2};
 	private static Player dealer = new Player();
 	
 	/*
 	 * Class variables
 	 */
 	private static int wager;
+	private static boolean splitCheck = false;
 	private static String ready;
 	private static String choice;
 	
@@ -67,11 +71,11 @@ public class Main {
 		dealHands();
 		System.out.print("\nDEALER'S HAND");
 		System.out.print("\n" + dealer.getHand().get(0).getRank() + " of " + dealer.getHand().get(0).getSuit());
-		printHand();
+		printHand(player);
 		if(dealer.sumHand() != 21) {
 			if(player.sumHand() != 21) {
-				playerTurn();
-				if(player.sumHand() <= 21) dealerTurn();
+				playerTurn(player);
+				dealerTurn();
 			}
 		}
 		
@@ -94,8 +98,12 @@ public class Main {
 	
 	/**
 	 * Player's turn.
+	 * <li> Stay: End turn and let dealer play their turn
+	 * <li> Hit: Player gets dealt another card
+	 * <li> Double: Player doubles their wager and gets dealt only 1 more card
+	 * <li> Split: If player has a pair, then they can't split their hand into two separate hands (with an equivalent wager on each)
 	 */
-	private static void playerTurn() {	
+	private static void playerTurn(Player p) {	
 		/*
 		 * Prompt choice until players "Stays"
 		 */
@@ -112,14 +120,28 @@ public class Main {
 			
 			// Hit
 			if(choice.equals("HIT")) {
-				player.addCard(deck.getNextCard());
-				printHand();
+				hit(p);
 			}
+			
+			// Double
+			if(choice.equals("DOUBLE")) {
+				if(dbl(p)) break;
+			}
+			
+			// Split
+			if(choice.equals("SPLIT")) {
+				if(p.getHand().get(0).getRank().equals(p.getHand().get(1).getRank())) {
+					split();
+					break;
+				} else {
+					System.out.print("\nSorry, you can't split unless you have a pair.");
+				}
+			}
+				
 			// Check for bust
 			if(player.sumHand() > 21) {
 				// Check for aces
 				if(!checkForAces()) {
-					printHand();
 					System.out.print("\nBUST!");
 					break;
 				}
@@ -135,7 +157,7 @@ public class Main {
 		/*
 		 * Print dealer's hand
 		 */
-		System.out.print("\n\n\nDEALER'S HAND: ");
+		System.out.print("\n\nDEALER'S HAND: ");
 		System.out.print("\n" + dealer.getHand().get(0).getRank() + " of " + dealer.getHand().get(0).getSuit());
 		System.out.print("\n" + dealer.getHand().get(1).getRank() + " of " + dealer.getHand().get(1).getSuit());
 
@@ -160,12 +182,12 @@ public class Main {
 	/**
 	 * Display the contents of the player's hand to the console.
 	 */
-	private static void printHand() {
+	private static void printHand(Player p) {
 		System.out.print("\n\nPLAYER'S HAND: ");
-		for(int i = 0; i < player.getHand().size(); i++) {
-			System.out.print("\n" + player.getHand().get(i).getRank() + " of " + player.getHand().get(i).getSuit());
+		for(int i = 0; i < p.getHand().size(); i++) {
+			System.out.print("\n" + p.getHand().get(i).getRank() + " of " + p.getHand().get(i).getSuit());
 		}
-		System.out.print("\nThis hand has a value of " + player.sumHand());
+		System.out.print("\nThis hand has a value of " + p.sumHand());
 	}
 	
 	/**
@@ -179,32 +201,63 @@ public class Main {
 		/*
 		 * Display winner
 		 */
-		if(dealer.sumHand() == 21 && dealer.getHand().size() == 2) {
-			player.setBank(-wager);
-			System.out.print("\nBLACKJACK! Dealer wins!");
-		} else if (player.sumHand() == 21 && player.getHand().size() == 2) {
-			player.setBank(wager);
-			System.out.print("\nBLACKJACK! Player wins!");
-		} else if (player.sumHand() > 21) {
-			player.setBank(-wager);
-			System.out.print("\nPlayer busted. Dealer wins!");
-		} else if (dealer.sumHand() > 21) {
-			player.setBank(wager);
-			System.out.print("\nDealer busted. Player wins!");
-		} else if (dealer.sumHand() > player.sumHand()) {
-			player.setBank(-wager);
-			System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + player.sumHand());
-			System.out.print("\nDealer wins!");
-		} else if (player.sumHand() > dealer.sumHand()) {
-			player.setBank(wager);
-			System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + player.sumHand());
-			System.out.print("\nPlayer wins!");
-		} else if (player.sumHand() == dealer.sumHand()) {
-			System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + player.sumHand());
-			System.out.print("\nDraw!");
+		if(!splitCheck) {
+			if(dealer.sumHand() == 21 && dealer.getHand().size() == 2) {
+				player.setBank(-wager);
+				System.out.print("\nBLACKJACK! Dealer wins!");
+			} else if (player.sumHand() == 21 && player.getHand().size() == 2) {
+				player.setBank(wager);
+				System.out.print("\nBLACKJACK! Player wins!");
+			} else if (player.sumHand() > 21) {
+				player.setBank(-wager);
+				System.out.print("\nPlayer busted. Dealer wins!");
+			} else if (dealer.sumHand() > 21) {
+				player.setBank(wager);
+				System.out.print("\nDealer busted. Player wins!");
+			} else if (dealer.sumHand() > player.sumHand()) {
+				player.setBank(-wager);
+				System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + player.sumHand());
+				System.out.print("\nDealer wins!");
+			} else if (player.sumHand() > dealer.sumHand()) {
+				player.setBank(wager);
+				System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + player.sumHand());
+				System.out.print("\nPlayer wins!");
+			} else if (player.sumHand() == dealer.sumHand()) {
+				System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + player.sumHand());
+				System.out.print("\nDraw!");
+			}
+		} else if (splitCheck) {
+			for(int i = 0; i < split.length; i++) {
+				System.out.print("\n\nFor hand #" + (i + 1));
+				
+				if(dealer.sumHand() == 21 && dealer.getHand().size() == 2) {
+					player.setBank(-wager);
+					System.out.print("\nBLACKJACK! Dealer wins!");
+				} else if (split[i].sumHand() == 21 && split[i].getHand().size() == 2) {
+					player.setBank(wager);
+					System.out.print("\nBLACKJACK! Player wins!");
+				} else if (split[i].sumHand() > 21) {
+					player.setBank(-wager);
+					System.out.print("\nPlayer busted. Dealer wins!");
+				} else if (dealer.sumHand() > 21) {
+					player.setBank(wager);
+					System.out.print("\nDealer busted. Player wins!");
+				} else if (dealer.sumHand() > split[i].sumHand()) {
+					player.setBank(-wager);
+					System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + split[i].sumHand());
+					System.out.print("\nDealer wins!");
+				} else if (split[i].sumHand() > dealer.sumHand()) {
+					player.setBank(wager);
+					System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + split[i].sumHand());
+					System.out.print("\nPlayer wins!");
+				} else if (split[i].sumHand() == dealer.sumHand()) {
+					System.out.print("\nDealer's hand: " + dealer.sumHand() + "\tPlayer's hand: " + split[i].sumHand());
+					System.out.print("\nDraw!");
+				}
+			}
 		}
 		
-		System.out.print("\n\nYou now have " + player.getBank() + " dollars in your bank");
+		System.out.print("\n\nYou now have $" + player.getBank() + " in your bank");
 	}
 	
 	/**
@@ -226,7 +279,7 @@ public class Main {
 	 * Let's player place a bet.
 	 */
 	private static void setWager() {
-		System.out.print("\nYou have " + player.getBank() + " dollars in your bank");
+		System.out.print("\nYou have $" + player.getBank() + " in your bank");
 		System.out.print("\nHow much would you like to bet? ");
 		wager = kb.nextInt();
 		
@@ -248,22 +301,28 @@ public class Main {
 			System.out.print("\nWould you like to play another round? Yes/No. ");
 			choice = kb.nextLine();
 			choice = choice.toUpperCase();
-			
-			if(choice.equals("YES")) {
-				resetHands();
-				return true;
+			while(true) {
+				if(choice.equals("YES")) {
+					resetRound();
+					return true;
+				} else if(choice.equals("NO")) {
+					break;
+				}
+				System.out.print("Sorry, please say Yes or No. ");
+				choice = kb.nextLine();
+				choice = choice.toUpperCase();
 			}
 		} else {
-			System.out.print("Sorry, you have run out money and can not play any more rounds.");
+			System.out.print("\nSorry, you have run out money and can not play any more rounds.");
 		}
 		
-		System.out.print("\nYou ended with " +  player.getBank() + " dollars in the bank.");
+		System.out.print("\nYou ended with $" +  player.getBank() + " in the bank.");
 		if(player.getBank() > 100) {
-			System.out.print(" That's a profit of " + (player.getBank() - 100) + " dollars.");
+			System.out.print(" That's a profit of $" + (player.getBank() - 100));
 		} else if (player.getBank() < 100) {
-			System.out.print(" That's a loss of " + (100 - player.getBank()) + " dollars.");
+			System.out.print(" That's a loss of $" + (100 - player.getBank()));
 		} else {
-			System.out.print(" That's a profit of " + (player.getBank() - 100) + " dollars.");
+			System.out.print(" That's a profit of $" + (player.getBank() - 100));
 		}
 		System.out.print("\n\n=======================================\n");
 		System.out.print("THANKS FOR PLAYING!");
@@ -275,8 +334,63 @@ public class Main {
 	/**
 	 * If player plays another round, remove all cards from dealer's and player's hands.
 	 */
-	private static void resetHands() {
+	private static void resetRound() {
 		dealer.getHand().clear();
 		player.getHand().clear();
+		split[0].getHand().clear();
+		split[1].getHand().clear();
+		splitCheck = false;
+	}
+	
+	/**
+	 * Hit
+	 */
+	private static void hit(Player p) {
+		p.addCard(deck.getNextCard());
+		printHand(p);
+	}
+	
+	/**
+	 * Double
+	 */
+	private static boolean dbl(Player p) {
+		if(p.getHand().size() == 2 && wager * 2 < p.getBank()) {	
+			p.addCard(deck.getNextCard());
+			wager *= 2;
+			System.out.print("\nYour new wager is $" + wager);
+			printHand(p);
+			
+			// Check for bust
+			if(player.sumHand() > 21) {
+				// Check for aces
+				if(!checkForAces()) {
+					System.out.print("\nBUST!");
+				}
+			}
+			
+			return true;
+		} else if (p.getHand().size() != 2) {
+			System.out.print("\nYou cannot double if you already drawn a card.");
+		} else if (wager * 2 > player.getBank()) {
+			System.out.print("\nSorry, you don't have the money to double your bet.");
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * "Split" action for player's turn
+	 */
+	private static void split() {
+		splitCheck = true;
+		for(int i = 0; i < split.length; i++) {
+			System.out.print("\n\nHand #" + (i + 1));
+			
+			split[i].addCard(player.getHand().get(i));
+			split[i].addCard(deck.getNextCard());
+			printHand(split[i]);
+			
+			playerTurn(split[i]);
+		}
 	}
 }
